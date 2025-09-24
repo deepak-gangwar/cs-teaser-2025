676e809345bcc2bf2df8100334e106f2634a87b2
0xshivam <shivamsharmashiv@outlook.com>
2025-09-24 16:08:43 +0530

    fix: setup bg and audio

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('it is working')
+// console.log('it is working')
+document.addEventListener('click', () => {
+    const music = document.getElementById('bg-music');
+    music.muted = false;
+    music.play().catch(e => console.log("Playback prevented:", e));
+    }, { once: true });
