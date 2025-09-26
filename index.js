a2409f611de9511f23296e96e13e949cdd46c4c1
Deepak <deepakgangwar4265@gmail.com>
2025-09-26 21:16:50 +0530

    update stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // console.log('it is working')
 document.addEventListener('click', () => {
-    const music = document.getElementById('bg-music');
+    const music = document.querySelector('.music');
     music.muted = false;
     music.play().catch(e => console.log("Playback prevented:", e));
     }, { once: true });
